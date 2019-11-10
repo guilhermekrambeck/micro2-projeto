d18cd643ac5c9846ef91aca560c8d19912842de5
Guilherme Krambeck Ferreira <guikferreira@outlook.com>
2019-11-10 12:45:47 -0300

    Fix reprint after command 10

--- v3/all.s
+++ v3/all.s
@@ -267,10 +267,10 @@ READ:									# Reading from keyboard using polling technique
 	movia   r5, KEYBOARD
 
 	ldwio   r9, 0(r5)					# R9 <- JTAG UART
-	and 	r10, r9, r3					# Verify availability [RVALID]
-	beq 	r10, r0, READ 				# If not avaiable, wait...
+	and 	r19, r9, r3					# Verify availability [RVALID]
+	beq 	r19, r0, READ 				# If not avaiable, wait...
 
-	and 	r10, r9, r4					# Get data from input when RVALID is available
+	and 	r19, r9, r4					# Get data from input when RVALID is available
 
 
 WRITE:									# Writing keyboard's input using polling technique
@@ -278,14 +278,14 @@ WRITE:									# Writing keyboard's input using polling technique
 	and 	r3, r2, r6					# Verify space availability [WSPACE]
 	beq 	r0, r3, WRITE				# While theres no space, wait...
 
-	stwio   r10, 0(r5)					# Print char on the terminal (using Data Register)
+	stwio   r19, 0(r5)					# Print char on the terminal (using Data Register)
 
 	movia   r4, ENTER_ASCII
-	beq 	r10, r4, EXECUTE			# If ENTER is hit, execute COMMAND
+	beq 	r19, r4, EXECUTE			# If ENTER is hit, execute COMMAND
 	movia   r4, BACKSPACE_ASCII
-	beq 	r10, r4, ERASE				# If BACKSPACE is hit, erase last char from memory
+	beq 	r19, r4, ERASE				# If BACKSPACE is hit, erase last char from memory
 
-	stw 	r10, 0(r8)					# Keep command value on memory
+	stw 	r19, 0(r8)					# Keep command value on memory
 	addi 	r8, r8, 4
 
 	br 		READ
@@ -298,14 +298,14 @@ EXECUTE:
 	movia   r8, LASTCMD
     ldw 	r9, 0(r8)					# Get the last command (first bit) in r9 (ASCII)
 	subi 	r9, r9, 0x30				# Convert from ASCII to integer
-	ldw 	r10, 4(r8)					# Get the last command (second bit) in r10 (ASCII)
-	subi 	r10, r10, 0x30				# Convert from ASCII to integer
+	ldw 	r19, 4(r8)					# Get the last command (second bit) in r19 (ASCII)
+	subi 	r19, r19, 0x30				# Convert from ASCII to integer
 
-	# Multiply r9 by 10 and add to r10 (making two (integer) bits into a decimal)
+	# Multiply r9 by 10 and add to r19 (making two (integer) bits into a decimal)
 	slli 	r11, r9, 3					# Since we are working with DE2 Media computer,
 	slli 	r12, r9, 1					# we could use "mul" operations
 	add 	r9, r11, r12
-	add 	r9, r9, r10					# R9 <- Final value
+	add 	r9, r9, r19					# R9 <- Final value
 
 	# Test which command user entered (simulation of c's switch structure)
 	addi 	r10, r0, LED_ON_FUNCTION
